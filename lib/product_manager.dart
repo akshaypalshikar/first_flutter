ec00c98e7a11408a501c9316c2f29dab54fe2ca9
Akshay Palshikar <aksmakesit@gmail.com>
2018-11-22 14:16:24 +0530

    Using special container expanded. It uses all the space available

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -47,7 +47,7 @@ class _ProductManagerState extends State<ProductManager> {
           margin: EdgeInsets.all(5.0),
           child: ProductControl(_addProduct),
         ),
-        Container(height:300.0,child:Products(_products)),
+        Expanded(child:Products(_products)),
       ],
     );
   }

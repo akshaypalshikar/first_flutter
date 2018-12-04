2d79f044286437a4f948564f3a27528b675521de
Akshay Palshikar <8948642+akshaypalshikar@users.noreply.github.com>
2018-12-04 17:11:27 +0530

    List view and optimization (#6)
    * ListView not working
    * using container with height 300. Remaining space not used
    * Using special container expanded. It uses all the space available
    * added builder for optimizing large list
    * Conditional rendering

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -22,7 +22,7 @@ class _ProductManagerState extends State<ProductManager> {
   @override
   void initState() {
     print('[ProductManager Widget] initState');
-    _products.add(widget.startingProduct);
+  //  _products.add(widget.startingProduct);
     super.initState();
   }
 
@@ -47,7 +47,7 @@ class _ProductManagerState extends State<ProductManager> {
           margin: EdgeInsets.all(5.0),
           child: ProductControl(_addProduct),
         ),
-        Products(_products),
+        Expanded(child:Products(_products)),
       ],
     );
   }
--- lib/products.dart
+++ lib/products.dart
@@ -7,22 +7,35 @@ class Products extends StatelessWidget {
     print('[Products Widget] Constructor');
   }
 
-  @override
-  Widget build(BuildContext context) {
-    print('[Products Widget] Build');
-    return Column(
-      children: products
-          .map(
-            (element) => Card(
+  Widget _buildProductItem(BuildContext context,int index){
+    return Card(
                   child: Column(
                     children: <Widget>[
                       Image.asset('assets/food.jpg'),
-                      Text(element),
+                      Text(products[index]),
                     ],
                   ),
-                ),
-          )
-          .toList(),
-    );
+                ); 
+  }
+
+  Widget _buildProductList(){
+    Widget productCards;
+      if(products.length>0){
+        productCards = ListView.builder(
+          itemBuilder: _buildProductItem,
+          itemCount: products.length
+        );
+      }
+      else{
+        productCards = Center(child: Text("No products found, please add products."));
+      }
+      return productCards;
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    print('[Products Widget] Build');
+    return _buildProductList();
   }
 }
